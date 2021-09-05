862f630141d60d21bba78c8293ad8717606f1876
Steve Cahill <cahillsf9@gmail.com>
2021-09-05 17:36:00 -0400

    import and init AOS and DD-RUM

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,31 @@
 </template>
 
 <script>
+import AOS from '../node_modules/aos/dist/aos.js';
+import { datadogRum } from '@datadog/browser-rum';
 export default {
-  name: 'App'
+  name: 'App',
+  beforeCreate() {
+    AOS.init();
+  },
+  created(){
+    datadogRum.init({
+          applicationId: '5c19ff1f-62dc-467c-8294-f1d54c6583fc',
+          clientToken: 'pub953963662d5f76b93bb0370a71113128',
+          site: 'datadoghq.com',
+          service:'vue-app',
+          // Specify a version number to identify the deployed version of your application in Datadog 
+          version: '1.0.0',
+          sampleRate: 100,
+          trackInteractions: true,
+          allowedTracingOrigins:["http://localhost:8000"]
+        });
+  }
 }
 </script>
 
 <style>
+@import '../node_modules/aos/dist/aos.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
